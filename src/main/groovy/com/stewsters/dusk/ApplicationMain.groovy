235e654fd792a7efcf3caf6669e4b893b34daa2a
Adrian Moore <stewsters@gmail.com>
2016-03-29 23:17:41 -0500

    fewer repaints

--- src/main/groovy/com/stewsters/dusk/ApplicationMain.groovy
+++ src/main/groovy/com/stewsters/dusk/ApplicationMain.groovy
@@ -64,23 +64,17 @@ public class ApplicationMain extends JFrame implements KeyListener, MouseInputLi
     @Override
     public void keyPressed(KeyEvent e) {
 
-
         screen = screen.respondToUserInput(e);
 
-        repaint();
         if (screen == null) {
             throw new NullPointerException("Screen must always return another one.");
         }
 
         while (screen.autoplay()) {
-
-            if (screen.play()) {
-                repaint();
-            }
-
+            screen.play()
         }
 
-
+        repaint();
     }
 
     @Override
