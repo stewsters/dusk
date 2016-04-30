834f95a15369d53290f00dc818baa3f01d6d83fb
Adrian Moore <stewsters@gmail.com>
2016-04-29 19:50:12 -0500

    Adding some daylight calcs

--- src/test/groovy/com/stewsters/test/LibraryTest.groovy
+++ src/test/groovy/com/stewsters/test/LibraryTest.groovy
@@ -1,13 +1,26 @@
 package com.stewsters.test
 
+import com.stewsters.dusk.game.Game
 import spock.lang.Specification
 
 class LibraryTest extends Specification {
-    def "someLibraryMethod returns true"() {
+    def "daylight looks ok"() {
+        given:
+        Game.gameTurn = gameTurn
 
         when:
-        def fish = true
+        def light = Game.dayLight
+
         then:
-        fish
+        light <= expectedLight + 0.0001
+        light >= expectedLight - 0.0001
+
+        where:
+        gameTurn                  | expectedLight
+        0                         | 0f
+        Game.DAY_LENGTH * (1 / 4) | 1f
+        Game.DAY_LENGTH * (2 / 4) | 0f
+        Game.DAY_LENGTH * (3 / 4) | -1f;
+        Game.DAY_LENGTH * (4 / 4) | 0f;
     }
 }
